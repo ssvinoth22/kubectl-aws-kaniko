0971161b94a38b4b35255103c54931d8cb4fc4c9
ssvinoth22 <ss_vinoth22@yahoo.co.in>
2020-05-12 19:41:20 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e
 
-aws eks --region ${CLUSTER_REGION} update-kubeconfig --name ${CLUSTER_NAME}
-
+# update kube config
+# aws configure set default.region ${CLUSTER_REGION}
+if [ -n "${EKS_CLUSTER_NAME}" ];then
+    aws eks --region ${CLUSTER_REGION} update-kubeconfig --name ${EKS_CLUSTER_NAME}
+fi
+# aws ECR configure
 eval $(aws ecr get-login --no-include-email --region ${CLUSTER_REGION})
+
+#aws ECS credential configure
+ecs-cli configure profile --profile-name ecs-profile
 
 exec "$@"
